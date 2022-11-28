045fdf9fd1395730a76a48bab34273093b0ef779
Pooja <pooja.naksskar@gmail.com>
2022-11-28 15:39:17 +0530

    test

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ data "aws_ami" "ubuntu" {
 */
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0b0dcb5067f052a63"
+  ami                    = "ami-094125af156557ca2"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
